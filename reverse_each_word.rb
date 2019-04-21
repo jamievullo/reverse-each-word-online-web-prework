078856262888309cc4da03f29ee88f9cf32e4e1d
Jamie Vullo <jvullo@protonmail.com>
2019-04-21 19:42:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence1)
   new_array = []
-  reverse_each_word.split.map do|word| 
-  new_array.reverse.join (" ")
+  reverse_each_word.each do|word| 
+  new_array.reverse
 end
 end
     
